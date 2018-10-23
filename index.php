46f966b7aab8c18b8fff76390d75aad06e55a53a
Ignitious44 <42798002+Ignitious44@users.noreply.github.com>
2018-10-23 09:31:21 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IJ portfolio</title>
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="Ignas Jurevicius">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="Ignas Jurevicius">
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+
+<section class="section1">
+    <div class="container-fluid">
+        <h1>Stylish Portfolio</h1>
+        <h3>A Free Bootstrap Theme by Start Bootstrap</h3>
+    </div>
+    <div class='cover'>
+        <button type="button" class="btn btn-primary btn-lg">Find Out More</button>
+    </div>
+</section>
+
+<section class="section2">
+    <div class="container-fluid">
+        <h3>Stylish Portfolio is the perfect theme for your next project!</h3>
+        <p>This theme features a flexible, UX friendly sidebar menu and stock photos from our friends from Unisplash</p>
+    </div>
+    <div class='cover'>
+        <button type="button" class="btn btn-secondary btn-lg">Find Out More</button>
+    </div>
+</section>
+
+<section class="section3">
+    <div class="container-fluid">
+        <h6>SERVICES<h6>    
+        <h2>What We Offer</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <article class='col-md-3'>
+                <div class='icon'>    
+                    <i class="fas fa-mobile-alt"></i>
+                </div>
+                <h4>Responsive</h4>
+                <p>Looks great on any screen size!</p>   
+            </article>
+            <article class='col-md-3'>
+                <div class='icon'>    
+                    <i class="fas fa-pencil-alt"></i>
+                </div>
+                <h4>Resdesigned</h4>
+                <p>Freshly redesigned for Bootstrap 4.</p>      
+            </article>
+            <article class='col-md-3'>
+                <div class='icon'>    
+                    <i class="far fa-thumbs-up"></i>
+                </div>
+                <h4>Favorited</h4>
+                <p>Millions of users love Start Bootstrap!</p>      
+            </article>
+            <article class='col-md-3'>
+                <div class='icon'>
+                    <i class="far fa-question-circle"></i>
+                </div>
+                <h4>Question</h4>
+                <p>I ask you a question...</p>      
+            </article>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
